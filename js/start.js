dd02df6ae52b5236a5bae992b2e5b748ea7dcac1
Noeunsol <nes70117011@gmail.com>
2023-04-16 23:50:14 +0900

    [MERGE] main branch

--- js/start.js
+++ js/start.js
@@ -1,27 +1,43 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
+const endPoint = 12;
 
-function addAnswer(answerText){
+function addAnswer(answerText, qIdx){
     var a = document.querySelector('.answerBox');
     var answer = document.createElement('button');
-    // answer.classList.add('answerList');
+    answer.classList.add('answerList');
+    answer.classList.add('my-3');
+    answer.classList.add('py-3');
+    answer.classList.add('mx-auto');
+    answer.classList.add('fadeIn');
+
     a.appendChild(answer);
     answer.innerHTML = answerText;
-    answer.aaddEventListener("click", function(){
+
+    answer.addEventListener("click", function(){
         var children = document.querySelectorAll('.answerList');
-        for(let i = 0; i < children.length; i++){
+        for(let i=0; i<children.length; i++){
             children[i].disabled = true;
-            children[i].style.display = 'none';
+            children[i].style.WebkitAnimation = "fadeOut 0.5s";
+            children[i].style.animation = "fadeOut 0.5s";
         }
+        setTimeout(() => {
+            for(let i=0; i<children.length; i++){
+                children[i].style.display = 'none';
+            }
+            goNext(++qIdx);
+        }, 450)
     }, false);
 }
 
-function goNext(){
+function goNext(qIdx){
     var q = document.querySelector('.qBox');
-    q.innerHTML = qnaList[0].q;
+    q.innerHTML = qnaList[qIdx].q;
     for(let i in qnaList[qIdx].a){
-        addAnswer(qnaList[qIdx].a[i].answer);
+        addAnswer(qnaList[qIdx].a[i].answer, qIdx);
     }
+    var status = document.querySelector('.statusBar');
+    status.style.width = (100/endPoint) * (qIdx+1) + '%';
 }
 
 function begin(){
@@ -33,10 +49,8 @@ function begin(){
         setTimeout(() => {
             main.style.display = "none";
             qna.style.display = "block";
-        }, 450);
+        }, 450)
         let qIdx = 0;
         goNext(qIdx);
     }, 450);
-    // main.style.display = "none";
-    // qna.style.display = "block";
 }
